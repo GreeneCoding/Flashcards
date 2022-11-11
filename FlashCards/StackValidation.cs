37193adec160d066473fa4a7a0cc34b1fda21714
greene <green@DESKTOP-9R3UJ0N>
2022-11-11 09:02:22 -0500

    Created Stack Validation, started building select statement for the Stacks table using SQLClient instead of Sqlite, but started with Sqlite, attempting to convert code to SqlClient. installed SqlClient and System.Configuration NuGet packages.

---
+++ FlashCards/StackValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class StackValidation
+    {
+        public static bool IsStackUnique()
+        {
+            bool result = false;
+
+            return result;
+        }
+    }
+}
--- FlashCards/StacksController.cs
+++ FlashCards/StacksController.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Flashcards
 {
     internal class StacksController
     {
         //TALKS TO THE DATABASE - Stacks Table
+        static string connectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        public static void ViewStacksTable()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = @"Select * from Stacks";
+                    tableCmd.ExecuteNonQuery();
+
+                    List<Stacks> stacksData = new();
+                    SqliteDataReader reader = tableCmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            stacksData.Add(new Stacks
+                            {
+                                Id = 
+                            }
+                            );
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
