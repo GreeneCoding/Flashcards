6dd5973ec087ada0f99bbe3cd610b65e30ba8d52
greene <green@DESKTOP-9R3UJ0N>
2022-11-28 07:09:42 -0500

    Staging ConsoleTableExt class

--- FlashCards/StacksController.cs
+++ FlashCards/StacksController.cs
@@ -1,91 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data.SqlClient;
 
-namespace Flashcards
+namespace Flashcards;
+
+internal class StacksController
 {
-    internal class StacksController
+    //TALKS TO THE DATABASE - Stacks Table, in the middle of refactoring SQLite code to SQLclient code
+    static string connectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+    public static void ViewStacksTable()
     {
-        //TALKS TO THE DATABASE - Stacks Table, in the middle of refactoring SQLite code to SQLclient code
-        static string connectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
-        public static void ViewStacksTable()
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var tableCmd = connection.CreateCommand())
             {
-                using (var tableCmd = connection.CreateCommand())
-                {
-                    connection.Open();
-                    tableCmd.CommandText = @"Select * from Stacks";
-                    tableCmd.ExecuteNonQuery();
+                connection.Open();
+                tableCmd.CommandText = @"Select * from Stacks";
+                tableCmd.ExecuteNonQuery();
 
-                    List<Stacks> stacksData = new();
-                    SqlDataReader reader = tableCmd.ExecuteReader();
-                    if (reader.HasRows)
+                List<Stacks> stacksData = new();
+                SqlDataReader reader = tableCmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        stacksData.Add(new Stacks
                         {
-                            stacksData.Add(new Stacks
-                            {
-                                Id = reader.GetInt32(0),
-                                StackName = reader.GetString(1)
-                            }
-                            );
+                            Id = reader.GetInt32(0),
+                            StackName = reader.GetString(1)
                         }
+                        );
                     }
-                    else
-                    {
-                        Console.WriteLine("No rows found.");
-                    }
-
                 }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
             }
         }
-        public static List<Stacks> ViewStacksbyName(string stackName)
+    }
+    public static List<Stacks> ViewStacksbyName(string stackName)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var tableCmd = connection.CreateCommand())
             {
-                using (var tableCmd = connection.CreateCommand())
-                {
-                    connection.Open();
-                    tableCmd.CommandText = @"SELECT StackName FROM Stacks WHERE @StackName = stackName";
-                    tableCmd.Parameters.AddWithValue("@StackName",stackName);
-                    tableCmd.ExecuteNonQuery();
+                connection.Open();
+                tableCmd.CommandText = @"SELECT StackName FROM Stacks WHERE @StackName = stackName";
+                tableCmd.Parameters.AddWithValue("@StackName",stackName);
+                tableCmd.ExecuteNonQuery();
 
-                    List<Stacks> stacksData = new();
-                    SqlDataReader reader = tableCmd.ExecuteReader();
-                    if (reader.HasRows)
+                List<Stacks> stacksData = new();
+                SqlDataReader reader = tableCmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        stacksData.Add(new Stacks
                         {
-                            stacksData.Add(new Stacks
-                            {
-                                StackName = reader.GetString(0)
-                            }
-                            );
+                            StackName = reader.GetString(0)
                         }
+                        );
                     }
-                    return stacksData;
-
                 }
+                return stacksData;
+
             }
         }
-        public static void CreateStacks(string stackName)
+    }
+    public static void CreateStacks(string stackName)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var tableCmd = connection.CreateCommand())
             {
-                using (var tableCmd = connection.CreateCommand())
-                {
-                    connection.Open();
-                    tableCmd.CommandText = @"INSERT INTO Stacks (StackName) VALUES (@StackName)";
-                    tableCmd.Parameters.AddWithValue("@StackName", stackName);
-                    tableCmd.ExecuteNonQuery();
-                }
+                connection.Open();
+                tableCmd.CommandText = @"INSERT INTO Stacks (StackName) VALUES (@StackName)";
+                tableCmd.Parameters.AddWithValue("@StackName", stackName);
+                tableCmd.ExecuteNonQuery();
             }
-
         }
+
     }
 }
---
+++ FlashCards/StacksVisualizationEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards;
+
+internal class StacksVisualizationEngine
+{
+    public void ShowStacksTable(List<Stacks> stacksData)
+    {
+
+    }
+}
